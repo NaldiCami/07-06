3257030bf12e3a4bbaf148d9b87a60e212276bc8
NaldiCami <123608227+NaldiCami@users.noreply.github.com>
2023-06-07 23:39:58 +0000

    bala

---
+++ trabalhoclasses/classes/GerenciarS3.py
@@ -0,0 +1,45 @@
+import os
+import boto3
+
+class GerenciarS3:
+    def __init__(self, nome_bucket):
+        self.nome_bucket = nome_bucket
+        self.s3 = boto3.client('s3')
+        
+    def lista_arquivos(self):
+        try:
+            resposta = self.s3.list_objects_v2(Bucket=self.nome_bucket)
+            
+            if 'Contents' in resposta:
+                files = [obj['Key'] for obj in resposta['Contents']]
+                print("Arquivos no S3:")
+                for file in files:
+                    print(file)
+            else:
+                print("Nenhum arquivo encontrado no S3.")
+        except Exception as e:
+            print(f"Erro ao listar os arquivos do S3: {e}")
+    def delete_arquivo(self, nome_arquivo):
+        try:
+            self.s3.delete_object(bucket=self.nome_bucket,key=nome_arquivo)
+            print(f'arquivo{nome_arquivo}excluido do s3 com sucesso.')
+        except exception as e:
+            print(f'erro ao excluir o arquivo do s3: {e}')
+
+    def upload_arquivo(self, caminho_arquivo, nome_arquivo=None):
+        if nome_arquivo is None:
+            nome_arquivo = caminho_arquivo
+
+        try:
+            self.s3.upload_file(caminho_arquivo, self.nome_bucket, nome_arquivo)
+            print(f'arquivo {nome_arquivo} enviadp para o S3 com sucesso.')
+        except exception as e:
+            print(f'erro ao enviar o arquivo para o S3: {e}')
+
+    def download_arquivo(self, nome_arquivo, caminho_arquivo):
+        try:
+            caminho_completo = os.path.join(caminho_arquivo, nome_arquivo)
+            self.s3.download_file(self.nome_bucket, nome_arquivo, caminho_completo)
+            print(f'arquivo {nome_arquivo} baixado do s3 com sucesso.')
+        except exception as e:
+            print(f'erro ao baixar o arquivo do s3: {e}')
